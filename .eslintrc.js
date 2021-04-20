0de32024d56d9c858397705f9d51979088888f98
lgmf <lgm.ferrari@gmail.com>
2021-04-20 20:20:44 -0300

    chore: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-native',
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'import/prefer-default-export': ['off'],
+  },
+};
